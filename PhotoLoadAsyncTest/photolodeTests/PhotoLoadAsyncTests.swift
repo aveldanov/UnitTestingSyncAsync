339f857a1c3825455dcb34c0e5c46e5635fcfa32
Anton Veldanov <anton.veldanov@gmail.com>
2020-06-06 17:07:07 -0700

    iOS

--- PhotoLoadAsyncTest/photolodeTests/PhotoLoadAsyncTests.swift
+++ PhotoLoadAsyncTest/photolodeTests/PhotoLoadAsyncTests.swift
@@ -13,13 +13,45 @@ import XCTest
 class PhotoLoadAsyncTests: XCTestCase {
   var sut: PhotoViewController!
     override func setUpWithError() throws {
+      
+      let storyboard = UIStoryboard(name: "Main", bundle: nil)
+      sut = storyboard.instantiateViewController(withIdentifier: "PhotoViewController") as! PhotoViewController
+      sut.loadViewIfNeeded()
     }
 
     override func tearDownWithError() throws {
-   
+   sut = nil
     }
 
-
+  func testPhotoDownLoadAsync_ImageOrientationIdentical(){
+    
+    let expectedImageOrientation = UIImage(named: "pexels-photo-768218")?.imageOrientation
+    
+    guard let url = URL(string: imageURLStrings[3]) else {
+      XCTFail();return
     }
+    let sessionAnsweredExpectation = expectation(description: "Session")
+    
+    URLSession.shared.dataTask(with: url) { (data, response, error) in
+      if let error = error{
+        XCTFail(error.localizedDescription)
+      }
+      
+      if let data = data {
+        
+        guard let image = UIImage(data: data) else {
+          XCTFail();return
+        }
+        sessionAnsweredExpectation.fulfill()
+        XCTAssertEqual(image.imageOrientation, expectedImageOrientation)
+        
+      }
+      
+    }.resume()
+    
+    waitForExpectations(timeout: 8, handler: nil)
+    
+  }
+    
 
 }
---
+++ README.md
@@ -0,0 +1,14 @@
+
+# Unit/Performance Testing using XCTestCase - Data Models, Features, EndPoints, MockData and Design Patterns
+
+
+## Sync Testing
+* Data Models
+* Views and Features Testing 
+
+
+## Async Tesing
+* waitLoad Images from API
+
+
+## Mocking Data
