9aabcf4fb92f0cc4ac9a4960203ff5e047d49bf4
Anton Veldanov <anton.veldanov@gmail.com>
2020-06-28 21:28:55 -0700

    iOS

---
+++ Phase2/photolode/photolode/Helpers/DownloadClient.swift
@@ -0,0 +1,28 @@
+
+
+import Foundation
+
+protocol SessionProtocol {
+    func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask
+}
+
+class DownloadClient {
+    var session: SessionProtocol = URLSession.shared
+    
+    func downloadImage(withURL url: String) {
+        guard let url = URL(string: url) else { fatalError() }
+        session.dataTask(with: url) { (data, response, error) in
+            //code to create image
+        }.resume()
+    }
+    
+    func terribleFunctionYouWouldNeverWrite() {
+        let totalValue = 1000000
+        var currentValue = 0
+        for i in 1...totalValue {
+            currentValue = i
+        }
+    }
+}
+
+extension URLSession: SessionProtocol {}
---
+++ Phase2/photolode/photolode/Model/PhotoCell.swift
@@ -0,0 +1,39 @@
+
+
+import UIKit
+
+var imageCache: NSCache<AnyObject, AnyObject> = NSCache()
+
+class PhotoCell: UITableViewCell {
+    @IBOutlet var photoImageView: UIImageView!
+    @IBOutlet weak var spinner: UIActivityIndicatorView!
+    
+    func downloadImage(withUrlString urlString: String) {
+        spinner.activityIndicatorViewStyle = .whiteLarge
+        spinner.hidesWhenStopped = true
+        spinner.startAnimating()
+
+        let url = URL(string: urlString)!
+        
+        if let imageFromCache = imageCache.object(forKey: url.absoluteString as AnyObject) as? UIImage {
+            self.photoImageView.image = imageFromCache
+            self.spinner.stopAnimating()
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url, completionHandler: { (data, response, error) in
+            
+            if error != nil {
+                debugPrint(String(describing: error?.localizedDescription))
+                return
+            }
+            
+            DispatchQueue.main.async {
+                let imageToCache = UIImage(data: data!)
+                self.photoImageView.image = imageToCache
+                self.spinner.stopAnimating()
+                imageCache.setObject(imageToCache!, forKey: url.absoluteString as AnyObject)
+            }
+        }).resume()
+    }
+}
