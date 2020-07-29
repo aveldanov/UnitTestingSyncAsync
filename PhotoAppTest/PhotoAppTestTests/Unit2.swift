c47799445a8473753ff9087dfe87b50d8c05be58
Anton Veldanov <anton.veldanov@gmail.com>
2020-07-28 21:58:16 -0700

    iOS

---
+++ PhotoAppTest/PhotoAppTestTests/Unit2.swift
@@ -0,0 +1,33 @@
+//
+//  Unit2.swift
+//  PhotoAppTestTests
+//
+//  Created by Veldanov, Anton on 7/28/20.
+//  Copyright © 2020 Anton Veldanov. All rights reserved.
+//
+
+import XCTest
+
+class Unit2: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample2() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
