ebbac76cfcce184dc289d25edbbcd49d7e82dadb
Anton Veldanov <anton.veldanov@gmail.com>
2020-08-07 23:45:53 -0700

    iOS

--- PhotoAppTest/PhotoAppTestTests/SignUp/Presenters/SignUpPresenterTests.swift
+++ PhotoAppTest/PhotoAppTestTests/SignUp/Presenters/SignUpPresenterTests.swift
@@ -43,7 +43,6 @@ class SignUpPresenterTests: XCTestCase {
         sut = nil
     }
     
-    
     func testSignupPresenter_WhenInformationProvided_WillValidateEachProperty(){
         // Arrange
 
