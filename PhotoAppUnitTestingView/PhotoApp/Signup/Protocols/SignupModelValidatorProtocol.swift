8d7632c7d61852ca58cd6a28fd67354d1b2c56e7
Anton Veldanov <anton.veldanov@gmail.com>
2020-09-14 11:55:22 -0700

    iOS

--- PhotoAppUnitTestingView/PhotoApp/Signup/Protocols/SignupModelValidatorProtocol.swift
+++ PhotoAppUnitTestingView/PhotoApp/Signup/Protocols/SignupModelValidatorProtocol.swift
@@ -14,7 +14,6 @@ protocol SignupModelValidatorProtocol {
       func isFirstNameValid(firstName: String) -> Bool
       
       func isLastNameValid(lastName: String) -> Bool
-      
       func isValidEmailFormat(email: String) -> Bool
       func isPasswordValid(password: String) -> Bool
       
